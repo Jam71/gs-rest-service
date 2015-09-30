4f2f1f8cc51354119a307bb4d3451cc9ee5c0716
Jamie <jamwiong@Gmail.com>
2015-09-30 14:45:56 -0600

    Changed to more info in contact

--- src/main/java/hello/Contact.java
+++ src/main/java/hello/Contact.java
@@ -6,16 +6,38 @@ import java.util.Collection;
 public class Contact {
 
 	private final long cid;
+	private final String firstName;
+	private final String lastName;
+	private final long phone;
+	private final String email;
 
 
-    public Contact(long cid) {
+    public Contact(long cid, String fName, String lName, long phone, String email) {
         this.cid = cid;
-      
+        this.firstName = fName;
+        this.lastName = lName;
+        this.phone = phone;
+        this.email = email;
     }
 
     public long getCId() {
         return cid;
     }
+    
+    public String getfName() {
+        return firstName;
+    }
+    
+    public String getlName() {
+        return lastName;
+    }
 
+    public long getPhone() {
+        return phone;
+    }
+    
+    public String getEmail() {
+        return email;
+    }
     
 }
