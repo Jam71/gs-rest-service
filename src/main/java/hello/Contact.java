705000a8965a619d987e305fe6fe1513900b8988
Jamie <jamwiong@Gmail.com>
2015-09-30 11:30:24 -0600

    first commit

---
+++ src/main/java/hello/Contact.java
@@ -0,0 +1,17 @@
+package hello;
+
+public class Contact {
+
+	private final long cid;
+
+
+    public Contact(long cid) {
+        this.cid = cid;
+      
+    }
+
+    public long getCId() {
+        return cid;
+    }
+
+}
