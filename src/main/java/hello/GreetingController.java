ca602ce05bbd90a8f11a522304d9ad3c9cdf1887
Jamie <jamwiong@Gmail.com>
2015-09-30 13:21:13 -0600

    Add contacts

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -8,8 +8,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.awt.List;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 
 @RestController
@@ -17,7 +18,7 @@ public class GreetingController {
 
 	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
-	//private Collection<Contact> contacts = (Collection<Contact>) new List();
+	private List<Contact> contacts = new ArrayList<Contact>();
 
 	@RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
@@ -27,15 +28,16 @@ public class GreetingController {
 	
     @RequestMapping("/contact")
     public Contact contact(@RequestParam(value="cid", defaultValue="0") long cid) {
-    	//Contact c1 = new Contact(cid);
-    	//contacts.add(c1);
-        return new Contact(cid);
+    	Contact c1 = new Contact(cid);
+    	contacts.add(c1);
+        return  c1;
     }
     
-//    @RequestMapping("/contacts")
-//	Collection<Contact> getContact(@RequestParam (value="targetCID", defaultValue="0")String targetCID) {
-//		return contacts;
-//	}
+    @RequestMapping("/contacts")
+	Collection<Contact> getContact(@RequestParam (value="targetCID", defaultValue="0")String targetCID) {
+		
+    	return contacts;
+	}
 //    @RequestMapping(method = RequestMethod.GET)
 //   	Collection<Contact> getContact(@RequestParam (value="cid", defaultValue="0")String cid) {
 //   		return getContact(cid);
