bfb7a588a2b6870c35db68b84f5c24849647e29d
Jamie <jamwiong@Gmail.com>
2015-09-30 13:48:54 -0600

    Set of contacts

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -10,7 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 
 @RestController
@@ -18,7 +21,8 @@ public class GreetingController {
 
 	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
-	private List<Contact> contacts = new ArrayList<Contact>();
+	//private Set contactsCID = new HashSet();
+	private List<Contact> contacts = new ArrayList<Contact>();	
 
 	@RequestMapping("/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
@@ -26,11 +30,25 @@ public class GreetingController {
                             String.format(template, name));
 	}
 	
+	// This should not be get, should be push
     @RequestMapping("/contact")
     public Contact contact(@RequestParam(value="cid", defaultValue="0") long cid) {
     	Contact c1 = new Contact(cid);
+    	// Iterate contacts list and if cid exist, delete the old one and update the new one
+    	for(Iterator<Contact> i = contacts.iterator(); i.hasNext(); )
+    	{
+    		Contact item = i.next();
+    		if(item.getCId() == cid)
+    		{
+    			//! I remember I can't change object inside iterator, need review on this part
+    			contacts.remove(item);
+    			contacts.add(c1);
+    			return c1;
+    		}
+    	}
+    	// If not exist, add it to the contacts list
     	contacts.add(c1);
-        return  c1;
+    	return  c1;
     }
     
     @RequestMapping("/contacts")
