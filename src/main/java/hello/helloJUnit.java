4525b403713fd2299d0403b505dff468b3ba791e
Jamie <jamwiong@Gmail.com>
2015-10-05 16:15:37 -0600

    Before adding JsonPath

--- src/main/java/hello/helloJUnit.java
+++ src/main/java/hello/helloJUnit.java
@@ -1,13 +1,14 @@
 package hello;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder.*;
-
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
 
 
 public class helloJUnit {
@@ -22,7 +23,12 @@ public class helloJUnit {
    	
 	@Test
 	public void test() throws Exception{
-		this.mockMvc.perform(get("/greeting","Jamie").accept(MediaType.APPLICATION_JSON))
+		this.mockMvc.perform(get("/greeting?name=Jamie").accept(MediaType.APPLICATION_JSON))
+//		.andExpect(status().isOk())
+//		.andExpect(content().json(("$.name").value("Jamie")))
+//		
+		.andDo(print())
+		.andExpect(status().isOk())
 		;
 		
 	}
