1afb8fc70fd5be969cafc32b3836766c864c9105
Jamie <jamwiong@Gmail.com>
2015-10-06 08:14:54 -0600

    This has the valid testing.

--- src/main/java/hello/helloJUnit.java
+++ src/main/java/hello/helloJUnit.java
@@ -6,6 +6,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
@@ -18,19 +19,64 @@ public class helloJUnit {
    	@Before
    	public void setUp() throws Exception
    	{
-   		mockMvc = MockMvcBuilders.standaloneSetup(new GreetingController()).build();	
+   		mockMvc = MockMvcBuilders.standaloneSetup(new GreetingController())
+   				.alwaysDo(print())
+   				.alwaysExpect(status().isOk())
+  				.alwaysExpect(content().contentType("application/json;charset=UTF-8"))
+   				.build();	
    	}
    	
 	@Test
-	public void test() throws Exception{
+	public void testGreeting() throws Exception{
 		this.mockMvc.perform(get("/greeting?name=Jamie").accept(MediaType.APPLICATION_JSON))
-//		.andExpect(status().isOk())
-//		.andExpect(content().json(("$.name").value("Jamie")))
-//		
-		.andDo(print())
-		.andExpect(status().isOk())
-		;
+		.andExpect(jsonPath("$.id").value(1))
+		.andExpect(jsonPath("$.content").value("Hello, Jamie!"));
 		
 	}
 
+	
+	@Test
+	public void testCreateContact() throws Exception{
+		//MockMultipartFile firstFile = new MockMultipartFile("json", "", "application/json", "{\"json\":\"obj.json\"}".getBytes());
+		this.mockMvc.perform(post(".").accept(MediaType.APPLICATION_JSON))
+		.andExpect(jsonPath("$.id").value(1))
+		.andExpect(jsonPath("$.content").value("Hello, Jamie!"));
+		
+	}
+	
+	@Test
+	public void testUpdateContact() throws Exception{
+		this.mockMvc.perform(get("/greeting?name=Jamie").accept(MediaType.APPLICATION_JSON))
+		.andExpect(jsonPath("$.id").value(1))
+		.andExpect(jsonPath("$.content").value("Hello, Jamie!"));
+		
+	}
+	
+	@Test
+	public void testGetContactID() throws Exception{
+		this.mockMvc.perform(get("/greeting?name=Jamie").accept(MediaType.APPLICATION_JSON))
+		.andExpect(jsonPath("$.id").value(1))
+		.andExpect(jsonPath("$.content").value("Hello, Jamie!"));
+		
+	}
+	
+	@Test
+	public void testGetContact() throws Exception{
+		this.mockMvc.perform(get("/greeting?name=Jamie").accept(MediaType.APPLICATION_JSON))
+		.andExpect(jsonPath("$.id").value(1))
+		.andExpect(jsonPath("$.content").value("Hello, Jamie!"));
+		
+	}
+	
+
+	@Test
+	public void testDeleteContact() throws Exception{
+		this.mockMvc.perform(get("/greeting?name=Jamie").accept(MediaType.APPLICATION_JSON))
+		.andExpect(jsonPath("$.id").value(1))
+		.andExpect(jsonPath("$.content").value("Hello, Jamie!"));
+		
+	}
+	
+
+	
 }
